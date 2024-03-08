a56e3ee977ebc3645368bc77218d71514382ad83
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-03-08 20:10:37 +0900

    BrendTree作成部分だけ見やすく変更/色々改名/保存をクラス分け/保存先変更

--- Editor/DBTPractice.cs
+++ Editor/DBTPractice.cs
@@ -1,123 +1,163 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using UnityEditor.Animations;
-using UnityEditor.VersionControl;
+using System;
+using System.IO;
 using System.Collections.Generic;
 
-using System;
-
-public class BlendTreeCreator : MonoBehaviour
+public class DBTPractice : MonoBehaviour
 {
     private const string PROJECTNAME = "DBTPractice";
-    private const string WEIGHT = "Weight";
-    private static string saveDirectory = $@"Assets/{PROJECTNAME}";
+    private const string ONE = "ONEf";
+    private static AnimatorController animatorController;
+    private static BlendTree BlendTree, BlendTree2;
 
     [MenuItem("DBT/make")]
-    static void CreateBlendTreeAsset()
+    static void createDBT()
     {
-        // Variable definition
-        var newController = new AnimatorController();
-        var newStateMachine = new AnimatorStateMachine();
-        var newState = new AnimatorState();
-        BlendTree BlendTree, BlendTree2;
-        AnimatorControllerParameter weightParameter;
-
-        // Setting Controller
-        newController.name = PROJECTNAME;
-
-        // Make Layer
-        newController.layers = new[]
-        {
-            new AnimatorControllerLayer
-            {
-                blendingMode = AnimatorLayerBlendingMode.Override,
-                defaultWeight = 1,
-                name = PROJECTNAME,
-                stateMachine = newStateMachine
-            }
-        };
-
-        // Make Parameter
-        weightParameter = new AnimatorControllerParameter()
-        {
-            name = WEIGHT,
-            type = AnimatorControllerParameterType.Float,
-            defaultFloat = 1
-        };
-        newController.AddParameter(weightParameter);
-
-        // Make Parent blendtree
+        // Make blendtree
         BlendTree = new BlendTree()
         {
             blendType = BlendTreeType.Direct,
-            blendParameter = WEIGHT,
             name = PROJECTNAME
         };
-
-        // Set BlendTree -> StateMachine
-        newStateMachine.name = PROJECTNAME;
-        newStateMachine.states = new[]
-        {
-            new ChildAnimatorState
-            {
-                state = newState,
-                position = Vector3.zero
-            }
-
-        };
-        newStateMachine.defaultState = newState;
-        newState.writeDefaultValues = true;
-        newState.motion = BlendTree;
-
-
-        // Make BlendTree2
         BlendTree2 = new BlendTree()
         {
-            blendType = BlendTreeType.Direct,
-            blendParameter = WEIGHT,
+            blendType = BlendTreeType.Simple1D,
+            blendParameter = ONE,
             name = $@"{PROJECTNAME}2"
         };
+        BlendTree.AddDirectChild(BlendTree2, ONE);
 
-        // Make Parent
-        BlendTree.AddChild(BlendTree2);
-        var children = BlendTree.children;
-        children[children.Length - 1].directBlendParameter = WEIGHT;
-        BlendTree.children = children;
-
-        // Save to File
-        SaveAssetToProjectFolder(newController);
-        SaveAssetToProjectFolder(BlendTree);
-        SaveAssetToProjectFolder(BlendTree2);
+        createAnimatorController();
+        PAssetsSave.run(PROJECTNAME, animatorController, BlendTree, BlendTree2);
     }
 
-
-    private static void SaveAssetToProjectFolder(UnityEngine.Object AST)
+    public static void createAnimatorController()
     {
-        if (!AssetDatabase.IsValidFolder(saveDirectory))
+        var animatorState = new AnimatorState()
         {
-            AssetDatabase.CreateFolder("Assets", PROJECTNAME);
-        }
-        string savePath=saveDirectory + $@"/{AST.name}.{extension(AST)}";
-        AssetDatabase.CreateAsset(AST, savePath);
-        AssetDatabase.SaveAssets();
-        Debug.Log($@"Asset Saved at: {savePath}");
-    }
-
-    private static string extension(UnityEngine.Object AST)
-    {
-        Dictionary<Type, string> extensionMap = new Dictionary<Type, string>
-        {
-            { typeof(BlendTree), "asset" },
-            { typeof(AnimatorController), "controller" },
+            writeDefaultValues = true,
+            motion = BlendTree,
+            name = "DBT",
         };
-        Type astType = AST.GetType();
-        if (extensionMap.ContainsKey(astType))
+        var animatorStateMachine = new AnimatorStateMachine()
         {
-            return extensionMap[astType];
-        }
-        else
+            name = PROJECTNAME,
+            states = new[]
+            {
+                new ChildAnimatorState
+                {
+                    state = animatorState,
+                }
+            },
+            defaultState = animatorState,
+        };
+        animatorController = new AnimatorController()
         {
-            return "asset";
+            name = PROJECTNAME,
+            layers = new[]
+            {
+                new AnimatorControllerLayer
+                {
+                    blendingMode = AnimatorLayerBlendingMode.Override,
+                    defaultWeight = 1,
+                    name = PROJECTNAME,
+                    stateMachine = animatorStateMachine
+                }
+            },
+        };
+        animatorController.AddParameter(
+            new AnimatorControllerParameter()
+            {
+                name = ONE,
+                type = AnimatorControllerParameterType.Float,
+                defaultFloat = 1
+            }
+        );
+    }
+
+}
+public static class BlendTreeExtensions
+{
+    public static void AddDirectChild(this BlendTree blendTree, BlendTree childTree, string parameterName)
+    {
+        if (blendTree == null)
+        {
+            throw new ArgumentNullException("blendTree");
         }
+        if (childTree == null)
+        {
+            throw new ArgumentNullException("childTree");
+        }
+        if (string.IsNullOrEmpty(parameterName))
+        {
+            throw new ArgumentNullException("parameterName");
+        }
+        blendTree.AddChild(childTree);
+        var c = blendTree.children;
+        c[c.Length - 1].directBlendParameter = parameterName;
+        blendTree.children = c;
+    }
+}
+public static class PAssetsSave 
+{
+    /// <summary>Pan Assets Save [[WARNING: This code will delete all files in the Gen folder without prior notice.]]</summary>
+    private static string projectName;
+    private static UnityEngine.Object[] assets;
+    private static UnityEngine.Object workingAsset;
+    private static Dictionary<Type, string> extensionMap = new Dictionary<Type, string>
+    {
+        { typeof(BlendTree), "asset" },
+        { typeof(AnimatorController), "controller" },
+    };
+    public static void run(string _projectName, params UnityEngine.Object[] _assets)
+    {
+        Debug.Log("This code is for development use only and is destructive. DO NOT USE in production.");
+        projectName = _projectName;
+        assets = _assets;
+        clearTempDirectory();
+        saveAssets();
+    }
+    private static string tempDirectory()
+    {
+        return $@"Assets/{projectName}/Temp/";
+    }
+    private static string fileName()
+    {
+        return $@"{workingAsset.name}.{extension()}";
+    }
+    private static string savePath()
+    {
+        return $@"{tempDirectory()}{fileName()}";
+    }
+    private static void clearTempDirectory()
+    {
+        if (Directory.Exists(tempDirectory()))
+        {
+            Directory.Delete(tempDirectory(), true);
+        }
+        Directory.CreateDirectory(tempDirectory());
+        AssetDatabase.Refresh();
+    }
+    private static void saveAssets()
+    {
+        foreach (var ast in assets)
+        {
+            workingAsset = ast;
+            if (AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(savePath()) != null)
+            {
+                Debug.LogWarning($@"Collision occurred in {savePath()}");
+            }
+            AssetDatabase.CreateAsset(workingAsset, savePath());
+            AssetDatabase.SaveAssets();
+            Debug.Log($@"Asset Saved at: {savePath()}");
+        }
+    }
+    private static string extension()
+    {
+        Type astType = workingAsset.GetType();
+        if (extensionMap.ContainsKey(astType)) return extensionMap[astType];
+        else return "asset";
     }
 }
